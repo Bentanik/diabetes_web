d9dfc29a4178704d3fdc539727bab1f3b68341cf
lamtanphat2202 <tphatt22@gmail.com>
2025-06-29 10:31:16 +0700

    add type for rp/rq get blogs

--- src/services/blog/typings.d.ts
+++ src/services/blog/typings.d.ts
@@ -1,5 +1,5 @@
 declare namespace REQUEST {
-    export type TCreateBlog = {
+    type TCreateBlog = {
         title: string;
         content: string;
         contentHtml: string;
@@ -8,6 +8,61 @@ declare namespace REQUEST {
         images: string[];
         doctorId: string;
     };
+
+    type BlogRequestParam = {
+        SearchContent: string;
+        CategoryIds: string[];
+        Status: number;
+        ModeratorId: string;
+        DoctorId: string;
+        IsAdmin: boolean;
+        PageIndex: number;
+        PageSize: number;
+        SortType: string;
+        IsSortAsc: boolean;
+    };
 }
 
-declare namespace API {}
+declare namespace API {
+    type Category = {
+        id: string;
+        name: string;
+        imageUrl: string;
+    };
+
+    // GET BLOG type
+    type Moderator = {
+        id: string;
+        fullName: string;
+        imageUrl: string;
+    };
+
+    type Doctor = {
+        id: string;
+        fullName: string;
+        imageUrl: string;
+    };
+
+    type Blog = {
+        id: string;
+        title: string;
+        thumbnail: string;
+        createdDate: string;
+        view: number;
+        like: number;
+        categories: Category[];
+        status: number;
+        moderator: Moderator;
+        doctor: Doctor;
+    };
+
+    type TGetBlogs = {
+        items: Blog[];
+        pageIndex: number;
+        pageSize: number;
+        totalCount: number;
+        totalPages: number;
+        hasNextPage: boolean;
+        hasPreviousPage: boolean;
+    };
+}
