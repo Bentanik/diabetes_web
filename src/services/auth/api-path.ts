b998d828321a8a0fd12d6292b2dbb1e2a583ce07
lamtanphat2202 <tphatt22@gmail.com>
2025-07-06 16:07:38 +0700

    pull master

--- src/services/auth/api-path.ts
+++ src/services/auth/api-path.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 const AUTH =
-  "https://capstonegateway-dqdrf9g0g9h0ffdd.southeastasia-01.azurewebsites.net/api/v1/auth";
+    "https://capstonegateway-dqdrf9g0g9h0ffdd.southeastasia-01.azurewebsites.net/api/v1/auth";
 const LOGIN = AUTH + "/system/login-email";
 const SEND_REGISTER_EMAIL = AUTH + "/send-register-email";
 const VERIFY_REGISTER_EMAIL = AUTH + "/verify-register-email";
@@ -8,9 +8,9 @@ const REGISTER_EMAIL = AUTH + "/register-email";
 const REFRESH_TOKEN = AUTH + "/refresh-token";
 
 export default {
-  LOGIN,
-  SEND_REGISTER_EMAIL,
-  VERIFY_REGISTER_EMAIL,
-  REGISTER_EMAIL,
-  REFRESH_TOKEN,
+    LOGIN,
+    SEND_REGISTER_EMAIL,
+    VERIFY_REGISTER_EMAIL,
+    REGISTER_EMAIL,
+    REFRESH_TOKEN,
 };
