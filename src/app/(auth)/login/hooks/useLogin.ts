2e98dbf25692b1b766129cdc87bc95222ee6bc28
Bentanik <kennyjamqw@gmail.com>
2025-08-11 00:20:00 +0700

    làm mẫu phần error

--- src/app/(auth)/login/hooks/useLogin.ts
+++ src/app/(auth)/login/hooks/useLogin.ts
@@ -1,7 +1,7 @@
 import { useBackdrop } from "@/context/backdrop_context";
 import {
-    loginSchema,
-    LoginSchemaFormData,
+  loginSchema,
+  LoginSchemaFormData,
 } from "@/lib/validations/auth.schema";
 import { useServiceLogin } from "@/services/auth/services";
 import { useAppDispatch } from "@/stores";
@@ -11,70 +11,66 @@ import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 
 export default function useLogin() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        setError,
-        setValue,
-        reset,
-    } = useForm<LoginSchemaFormData>({
-        resolver: zodResolver(loginSchema),
-        defaultValues: {
-            email: "",
-            password: "",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    setValue,
+    reset,
+  } = useForm<LoginSchemaFormData>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const { mutate } = useServiceLogin();
+  const { showBackdrop, hideBackdrop } = useBackdrop();
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+
+  const onSubmit = async (data: LoginSchemaFormData) => {
+    try {
+      const request: REQUEST.TLogin = {
+        email: data?.email,
+        password: data.password,
+      };
+
+      showBackdrop();
+      mutate(request, {
+        onSuccess: async (data) => {
+          if (data) {
+            hideBackdrop();
+            reset();
+            dispatch(clearAllRegister());
+            router.push("/admin/home");
+          }
         },
-    });
+        onError: (error) => {
+          hideBackdrop();
+          error.errorCodes?.forEach((error) => {
+            if (error.code === "auth_error_13") {
+              setError("email", {
+                type: "manual",
+                message: error.message,
+              });
+            }
+          });
+        },
+      });
+    } catch (err) {
+      console.log("err: ", err);
+    }
+  };
 
-    const { mutate } = useServiceLogin();
-    const { showBackdrop, hideBackdrop } = useBackdrop();
-    const router = useRouter();
-    const dispatch = useAppDispatch();
-
-    const onSubmit = async (data: LoginSchemaFormData) => {
-        try {
-            const request: REQUEST.TLogin = {
-                email: data?.email,
-                password: data.password,
-            };
-
-            showBackdrop();
-            mutate(request, {
-                onSuccess: async (data) => {
-                    if (data) {
-                        hideBackdrop();
-                        reset();
-                        dispatch(clearAllRegister());
-                        router.push("/admin/home");
-                    }
-                },
-                onError: (error) => {
-                    hideBackdrop();
-                    if (error.title === "auth13") {
-                        setError("email", {
-                            type: "manual",
-                            message:
-                                "Xin vui lòng kiểm tra lại địa chỉ email và mật khẩu",
-                        });
-                        setError("password", {
-                            type: "manual",
-                            message:
-                                "Xin vui lòng kiểm tra lại địa chỉ email và mật khẩu",
-                        });
-                    }
-                },
-            });
-        } catch (err) {
-            console.log("err: ", err);
-        }
-    };
-
-    return {
-        register,
-        handleSubmit,
-        errors,
-        onSubmit,
-        setValue,
-        setError,
-    };
+  return {
+    register,
+    handleSubmit,
+    errors,
+    onSubmit,
+    setValue,
+    setError,
+  };
 }
