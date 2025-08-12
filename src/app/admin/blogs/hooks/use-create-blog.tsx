a9c1249f5245b4b4e7f2f336d702a777011fc90e
lamtanphat2202 <tphatt22@gmail.com>
2025-08-12 15:22:17 +0700

    fix create post

--- src/app/admin/blogs/hooks/use-create-blog.tsx
+++ src/app/admin/blogs/hooks/use-create-blog.tsx
@@ -15,14 +15,13 @@ export default function useCreateBlog() {
         showBackdrop();
         mutate(undefined, {
             onSuccess: async (res) => {
-                await queryClient.invalidateQueries({
-                    queryKey: [GET_POSTS_QUERY_KEY],
-                });
                 const blogId = res.data?.id;
-                console.log(blogId);
                 if (blogId) {
                     router.push(`/admin/blogs/update-blog/${blogId}`);
                 }
+                await queryClient.invalidateQueries({
+                    queryKey: [GET_POSTS_QUERY_KEY],
+                });
                 hideBackdrop();
             },
             onError: (err) => {
